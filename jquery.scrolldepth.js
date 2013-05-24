082772027fe9f9b0aed6d9a8b80f419a65ccf2f5
Mike Schierberl <mike@schierberl.com>
2013-05-23 17:39:54 -0700

    Convert analytics event tracking to use new analytics.js syntax

--- jquery.scrolldepth.js
+++ jquery.scrolldepth.js
@@ -43,10 +43,10 @@
     function sendEvent(action, label, timing) {
       if (!options.testing) {
 
-        _gaq.push(['_trackEvent', 'Scroll Depth', action, label, 1, true]);
+        ga('send', 'event', 'Scroll Depth', action, label, 1, {'nonInteraction': 1});
 
         if (arguments.length > 2) {
-          _gaq.push(['_trackTiming', 'Scroll Depth', action, timing, label, 100]);
+          ga('send', 'timing', 'Scroll Depth', action, timing, label);
         }
 
       } else {
